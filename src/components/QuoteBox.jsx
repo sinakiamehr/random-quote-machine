fd973b6a23f76ab1481f842ca6e88aeddf3c54ce
Sina Kiamehr <sinakiamehr7@gmail.com>
2025-03-10 22:10:11 -0700

    created text box and generator

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const QuoteBox = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const onClickHandler = () => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      })
+      .catch((error) => console.error("Error fetching the quote:", error));
+  };
+
+  return (
+    <div className="quote-box" id="quote-box">
+      <p className="quote-text" id="text">
+        "{quote}""
+      </p>
+      <p className="quote-author" id="author">
+        -{author}
+      </p>
+      <button className="new-btn" id="new-quote" onClick={onClickHandler}>
+        Generate New Quote
+      </button>
+      <a
+        className="tweet-a"
+        target="_blank"
+        id="tweet-quote"
+        href="twitter.com/intent/tweet"
+      >
+        Tweet this quote
+      </a>
+    </div>
+  );
+};
+
+export default QuoteBox;
